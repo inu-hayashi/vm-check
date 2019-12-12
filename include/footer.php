254b46f959b73b6be6c475f7a2682ea571336207
mori <webtourokuyouid@gmail.com>
2019-12-12 18:52:14 +0900

    footerの連絡先の変更

--- include/footer.php
+++ include/footer.php
@@ -3,7 +3,7 @@
    <div class="row">
      <div class="col-10 mx-auto">
        <h3 class="footer-title">連絡先</h3>
-       <p>このサイトについてのご質問や要望は<a href="https://twitter.com/inudaihuku" target="_blank">こちら</a>へご連絡ください。(商品については各メーカーにお問い合わせください。)</p>
+       <p>このサイトについてのご質問や要望は<a href="http://mofolio.starfree.jp/service/vm/contact.php" target="_blank">こちら</a>へご連絡ください。(商品については各メーカーにお問い合わせください。)</p>
        <p class="text-center">Copyright © 自販売機の原材料・甘味料チェッカー</p>
      </div>
    </div>
